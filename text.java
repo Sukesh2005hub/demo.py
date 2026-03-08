3c42427387f30ddb2fe7a8b844b7c8d52ea0802e
Sukeshkrishna E <sukeshkrishnae2005@gmail.com>
2026-03-08 21:48:18 +0530

    Update text.java

--- text.java
+++ text.java
@@ -1,19 +1,25 @@
 public class Co {
-  public Co (){
-     
-    System.out.println("constructor with empty parameter");
-  }
-  public Co
-    System.out.println("constructor with a "+a);
-  }
-  public Co(float a, int c){
-     System.out.println("constructor with a "+a +" "+c);
-  }
-public Co(int c,int a){
-  System.out.println("Constructor with a "+a +" "+c);
+
+    public Co() {
+        System.out.println("constructor with empty parameter");
+    }
+
+    public Co(int a) {
+        System.out.println("constructor with a " + a);
+    }
+
+    public Co(float a, int c) {
+        System.out.println("constructor with a " + a + " " + c);
+    }
+
+    public Co(int c, int a) {
+        System.out.println("Constructor with a " + a + " " + c);
+    }
+
+    public static void main(String[] args) {
+        new Co();
+        new Co(2f, 7);
+        new Co(4, 8);
+        new Co(3);
+    }
 }
-public static void main(String[] args) {
-  new Co();
-  new Co(2f,7);
-  new Co(4,8);
-  new Co(3);
